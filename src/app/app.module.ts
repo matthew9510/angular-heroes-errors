7b71fcace6fa0262944d46dea240df50a386e1db
matthew9510@gmail.com <matthew9510@gmail.com>
2019-08-01 16:24:31 -0700

    fixed messages component to load properly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component'
 
 
 @NgModule({
